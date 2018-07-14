afb2eb8d7537b8248607210b343aa3450d93f14a
KM <sodp2@naver.com>
2018-07-15 03:27:53 +0900

    Modify the redux store

--- src/store/configure.js
+++ src/store/configure.js
@@ -1,9 +1,13 @@
+// redux store를 생성하는 함수를 만들어서 내보냄
+
 import { createStore } from 'redux';
 import modules from './modules';
 
 const configure = () => {
-  const store = createStore(modules);
-  return store;
+    const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    const store = createStore(modules, devTools);
+
+    return store;
 }
 
 export default configure;
---
+++ src/store/index.js
@@ -0,0 +1,4 @@
+// configure를 이용하여 redux store를 만들고 내보냄
+
+import configure from './configure'
+export default configure()
