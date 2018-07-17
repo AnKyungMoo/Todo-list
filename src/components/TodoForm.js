c689a49d63ce67acec5a5d527aac2dc7310dacd7
KM <sodp2@naver.com>
2018-07-17 21:43:45 +0900

    Add the insert function to EnterKey

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,13 +3,19 @@ import './TodoForm.css'
 
 class TodoForm extends Component {
     
+    handleEnter = (e) => {
+        if(e.keyCode === 13) {
+            this.props.onInsert();
+        }
+    }
+
     render() {
         const { value, onInsert, onChange } = this.props
 
         return(
             <div className='title'>
                 <div>Todo-List</div>
-                <input value={ value } onChange={ onChange }/>
+                <input value={ value } onChange={ onChange } onKeyDown={ this.handleEnter }/>
                 <button onClick={ onInsert }>+</button>
             </div>
         )
